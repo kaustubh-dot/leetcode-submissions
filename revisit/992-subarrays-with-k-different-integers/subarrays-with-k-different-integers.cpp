f39a16f98956afeb6b9678fb98e516c686c3dde4
kaustubh-dot <kaustubhupadhya0@gmail.com>
2026-08-03 19:07:16 +0530

    Time: 81 ms (62.99%) | Memory: 56.3 MB (39.53%) - CodeSync

---
+++ revisit/992-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int k) {
+        int low = 0, high = 0;
+        int count = 0;
+        unordered_map<int, int> m;
+        while (high < nums.size()) {
+            m[nums[high]]++;
+            while (m.size() > k) {
+                m[nums[low]]--;
+                if (m[nums[low]] == 0) {
+                    m.erase(nums[low]);
+                }
+                low++;
+            }
+            count += high - low + 1;
+            high++;
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return solve(nums,k)-solve(nums,k-1);
+    }
+};
