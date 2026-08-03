417ff7b0528ad0d369d938bef8b94d34e057672f
kaustubh-dot <kaustubhupadhya0@gmail.com>
2026-08-03 19:27:17 +0530

    Time: 7 ms (67.29%) | Memory: 71.4 MB (70.10%) - CodeSync

--- revisit/1248-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ revisit/1248-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -3,18 +3,12 @@ public:
     int solve(vector<int>& nums, int k){
         int low=0,high=0;
         int count=0;
-        unordered_map<int,int> m;
         int ans=0;
         while(high<nums.size()){
-            m[nums[high]]++;
             if(nums[high]%2==1){
                 count++;
             }
             while(count>k){
-                m[nums[low]]--;
-                if(m[nums[low]]==0){
-                    m.erase(nums[low]);
-                }
                 if(nums[low]%2==1){
                     count--;
                 }
