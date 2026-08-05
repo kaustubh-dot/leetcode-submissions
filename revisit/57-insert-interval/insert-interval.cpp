c577a42371c682f6fb4461bddd4a94cc65f3b8fe
kaustubh-dot <kaustubhupadhya0@gmail.com>
2026-08-05 22:25:23 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (22.63%) - CodeSync

---
+++ revisit/57-insert-interval/insert-interval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& neww) {
+        vector<vector<int>> ans;
+        int start1=neww[0];
+        int end1=neww[1];
+        for(int i=0;i<intervals.size();i++){
+            int start2=intervals[i][0];
+            int end2=intervals[i][1];
+            if(end2<start1){
+                ans.push_back({start2,end2});
+                continue;
+            }
+            if(start2<=end1){
+                start1=min(start1,start2);
+                end1=max(end1,end2);
+                continue;
+            }
+            ans.push_back({start1,end1});
+            start1=start2;
+            end1=end2;
+        }
+        ans.push_back({start1,end1});
+        return ans;
+    }
+};
