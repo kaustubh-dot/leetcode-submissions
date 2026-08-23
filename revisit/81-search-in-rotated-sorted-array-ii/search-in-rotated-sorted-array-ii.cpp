158db079632a4d0cac434b669288ce488ffdba37
kaustubh-dot <kaustubhupadhya0@gmail.com>
2026-08-23 15:03:55 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (46.81%) - CodeSync

---
+++ revisit/81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low = 0;
+        int n = nums.size();
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if(nums[mid]==nums[high]){
+                high--;
+                continue;
+            }
+            if (nums[mid] > nums[high]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+            return false;
+    }
+};
