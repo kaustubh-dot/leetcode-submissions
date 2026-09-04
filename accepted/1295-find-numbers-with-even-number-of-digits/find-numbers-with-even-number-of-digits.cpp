259e35ce266c0f8c80f23ced797aeaac0b02d77d
kaustubh-dot <kaustubhupadhya0@gmail.com>
2026-09-04 21:22:25 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (8.61%) - CodeSync

---
+++ accepted/1295-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count=0;
+        for(auto &i:nums){
+            string s=to_string(i);
+            if(s.size()%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
