98cc515f5085010df0bbc915dcba8404858ed0d7
kaustubh-dot <kaustubhupadhya0@gmail.com>
2026-09-24 21:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (52.36%) - CodeSync

---
+++ accepted/3550-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    int helper(int num){
+        int sum=0;
+        while(num>0){
+            sum+=num%10;
+            num/=10;
+        }
+        return sum;
+    }
+    int smallestIndex(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            int x=helper(nums[i]);
+            if(x==i){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
