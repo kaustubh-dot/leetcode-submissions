8ecd2810a45e87083a0fd5912979739d8c61f66a
kaustubh-dot <kaustubhupadhya0@gmail.com>
2026-09-25 15:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (89.78%) - CodeSync

---
+++ accepted/728-self-dividing-numbers/self-dividing-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for(int i=left;i<=right;i++){
+            int temp=i;
+            bool exist=true;
+            while(temp>0){
+                int digit=temp%10;
+                if(digit==0 || i%digit!=0){
+                    exist=false;
+                    break;
+                }
+                temp/=10;
+            }
+            if(exist){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
